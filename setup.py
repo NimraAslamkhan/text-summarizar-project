117f60939ed5c265508e725ece6f0e386f57e457
“nimraaslam3132@gmail.com <nimraaslam3132@gmail.com>
2024-05-09 10:17:14 +0500

    text summarizar

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "NimraAslamkhan"
+REPO_NAME = "text-summarizar-project"
+AUTHOR_USER_NAME = "NimraAslamKhan"
 SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "nimraaslam3132@gmail.com"
 
@@ -16,14 +16,14 @@ AUTHOR_EMAIL = "nimraaslam3132@gmail.com"
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
-    author=NimraAslamkhan,
-    author_email=nimraaslam3132@gmail.com,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     description="A small python package for NLP app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/NimraAslamkhan/text-summarizar-project.git",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/NimraAslamkhan/text-summarizar-project.git",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
